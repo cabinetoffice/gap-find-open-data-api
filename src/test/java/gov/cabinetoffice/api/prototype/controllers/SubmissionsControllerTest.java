1e5f0b372841b517d3f64dff02ed3828ab3fd3b9
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-06-12 12:58:46 +0100

    fix test

--- src/test/java/gov/cabinetoffice/api/prototype/controllers/SubmissionsControllerTest.java
+++ src/test/java/gov/cabinetoffice/api/prototype/controllers/SubmissionsControllerTest.java
@@ -10,15 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -30,23 +30,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = { SubmissionsController.class, ControllerExceptionsHandler.class })
 class SubmissionsControllerTest {
 
+    private static final String BASE_PATH = "/submissions/";
     private final Integer APPLICATION_ID = 1;
-
     @Autowired
     private MockMvc mockMvc;
-
     @MockBean
     private SubmissionsService submissionsService;
-
     @Autowired
     private ObjectMapper objectMapper;
 
-    private static final String BASE_PATH = "/submissions/";
-
     @Test
     void getSubmissionByApplicationId_found() throws Exception {
-        ApplicationFormEntity applicationForm = ApplicationFormEntity.builder().grantApplicationId(APPLICATION_ID)
-                .applicationName("test").grantApplicationId(APPLICATION_ID).build();
+        ApplicationFormEntity applicationForm = ApplicationFormEntity.builder()
+            .grantApplicationId(APPLICATION_ID)
+            .applicationName("test")
+            .grantApplicationId(APPLICATION_ID)
+            .build();
 
         Submission submission = Submission.builder().application(applicationForm).build();
         List<Submission> submissions = List.of(submission);
@@ -55,7 +54,8 @@ class SubmissionsControllerTest {
         String expectedJson = objectMapper.writeValueAsString(submissions);
 
         mockMvc.perform(get(BASE_PATH + APPLICATION_ID).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(content().json(expectedJson));
+            .andExpect(status().isOk())
+            .andExpect(content().json(expectedJson));
     }
 
     @Test
@@ -63,17 +63,28 @@ class SubmissionsControllerTest {
         String errorMsg = "No submissions found with application id " + APPLICATION_ID;
 
         when(submissionsService.getSubmissionByApplicationId(APPLICATION_ID))
-                .thenThrow(new SubmissionNotFoundException(errorMsg));
+            .thenThrow(new SubmissionNotFoundException(errorMsg));
 
         mockMvc.perform(get(BASE_PATH + APPLICATION_ID).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound()).andExpect(jsonPath("$.message").value(errorMsg));
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.message").value(errorMsg));
     }
 
     @Test
     void getSubmissionByApplicationId_invalidArgument() throws Exception {
-        mockMvc.perform(get(BASE_PATH + "invalidParameterType"))
-                .andExpect(status().isBadRequest())
-                .andExpect(result -> result.getResponse().getErrorMessage().contains("Incorrect parameter type passed"));
+        ResultActions actions = mockMvc.perform(get(BASE_PATH + "invalidParameterType"));
+
+        String errorMessage = actions
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        int errorCode = actions.andReturn().getResponse().getStatus();
+        assertThat(errorCode).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        assertThat(errorMessage).contains("Incorrect parameter type passed");
+
+
+
+
     }
 
 }
