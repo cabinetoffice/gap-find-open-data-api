a5446773e3bc32f25e384db105909666f4560124
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-06-22 11:31:18 +0100

    adds AwsConfig

---
+++ src/main/java/gov/cabinetoffice/api/prototype/config/AwsClientConfig.java
@@ -0,0 +1,28 @@
+package gov.cabinetoffice.api.prototype.config;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Configuration("awsClientConfigurationProperties")
+@ConfigurationProperties(prefix = "aws")
+public class AwsClientConfig {
+
+    @NotNull
+    private String secretKey;
+
+    @NotNull
+    private String accessKey;
+
+    @NotNull
+    private String region;
+
+}
