d53eb0016acd7d8903f107df6c9690e1e252302f
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2023-07-03 11:50:19 +0100

    Fixing broken unit test

--- src/test/java/gov/cabinetoffice/api/prototype/services/S3ServiceTest.java
+++ src/test/java/gov/cabinetoffice/api/prototype/services/S3ServiceTest.java
@@ -1,11 +1,13 @@
 package gov.cabinetoffice.api.prototype.services;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
@@ -13,11 +15,6 @@ import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequ
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class S3ServiceTest {
@@ -32,16 +29,15 @@ class S3ServiceTest {
 	private S3Service s3Service;
 
 	private final String BUCKET_NAME = "test-bucket-name";
+
 	private final String OBJECT_KEY = "test-object-key";
 
 	@Test
 	void testCreatePresignedURL() throws MalformedURLException {
 		final URL url = new URL("https://a-pre-signed-url.com");
 
-		when(s3PresignerMock.presignGetObject(Mockito.any(GetObjectPresignRequest.class)))
-				.thenReturn(presignedRequest);
-		when(presignedRequest.url())
-				.thenReturn(url);
+		when(s3PresignerMock.presignGetObject(Mockito.any(GetObjectPresignRequest.class))).thenReturn(presignedRequest);
+		when(presignedRequest.url()).thenReturn(url);
 
 		final String resultUrl = s3Service.createPresignedURL(BUCKET_NAME, OBJECT_KEY);
 
