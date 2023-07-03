7a23bcc2517aeecf868b266a6ad5ce69264d916f
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2023-07-03 11:45:52 +0100

    Fixing broken unit test

--- src/test/java/gov/cabinetoffice/api/prototype/services/S3ServiceTest.java
+++ src/test/java/gov/cabinetoffice/api/prototype/services/S3ServiceTest.java
@@ -4,11 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedGetObjectRequest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,31 +25,28 @@ class S3ServiceTest {
 	@Mock
 	private S3Presigner s3PresignerMock;
 
+	@Mock
+	private PresignedGetObjectRequest presignedRequest;
+
 	@InjectMocks
 	private S3Service s3Service;
 
 	private final String BUCKET_NAME = "test-bucket-name";
-
-	final String REGION = "eu-west-2";
-
 	private final String OBJECT_KEY = "test-object-key";
 
 	@Test
-	void testCreatePresignedURL() {
-		final String EXPECTED_URL_START = "https://" + BUCKET_NAME + ".s3." + REGION + ".amazonaws.com/" + OBJECT_KEY
-				+ "?";
+	void testCreatePresignedURL() throws MalformedURLException {
+		final URL url = new URL("https://a-pre-signed-url.com");
 
-		final S3Presigner s3Presigner = S3Presigner.builder().build();
-
-		final PresignedGetObjectRequest presigned = s3Presigner.presignGetObject(
-				getObjectPresignerRequest -> getObjectPresignerRequest.signatureDuration(Duration.ofMinutes(15))
-					.getObjectRequest(getObjectRequest -> getObjectRequest.bucket(BUCKET_NAME).key(OBJECT_KEY)));
-
-		when(s3PresignerMock.presignGetObject(any(GetObjectPresignRequest.class))).thenReturn(presigned);
+		when(s3PresignerMock.presignGetObject(Mockito.any(GetObjectPresignRequest.class)))
+				.thenReturn(presignedRequest);
+		when(presignedRequest.url())
+				.thenReturn(url);
 
 		final String resultUrl = s3Service.createPresignedURL(BUCKET_NAME, OBJECT_KEY);
 
-		assertThat(resultUrl).startsWith(EXPECTED_URL_START);
+		verify(s3PresignerMock).presignGetObject(Mockito.any(GetObjectPresignRequest.class));
+		assertThat(resultUrl).isEqualTo(url.toString());
 	}
 
 }
