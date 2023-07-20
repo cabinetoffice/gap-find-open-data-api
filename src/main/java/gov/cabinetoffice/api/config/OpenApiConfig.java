b752b1987c5a594814c4a6f4e26ca83a48677762
Connor26 <connor-26@hotmail.co.uk>
2023-07-20 15:49:29 +0100

    Some more text/word changes after project name change

--- src/main/java/gov/cabinetoffice/api/config/OpenApiConfig.java
+++ src/main/java/gov/cabinetoffice/api/config/OpenApiConfig.java
@@ -5,8 +5,8 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "OpenApi Prototype",
-		description = "Prototype to give an idea on what is achievable", version = "4.0"))
+@OpenAPIDefinition(info = @Info(title = "GAP Find Open Data API",
+		description = "API for retrieving application form data from the admin database.", version = "4.0"))
 public class OpenApiConfig {
 
 }
