43d541428c8d374ef7a9134084f20ef868098290
rachelswart <rachel.swart@cabinetoffice.gov.uk>
2023-07-28 14:01:35 +0100

    removed runtime exception from exceptionHandlerFilter

--- src/main/java/gov/cabinetoffice/api/security/ExceptionHandlerFilter.java
+++ src/main/java/gov/cabinetoffice/api/security/ExceptionHandlerFilter.java
@@ -1,10 +1,6 @@
 package gov.cabinetoffice.api.security;
 
-import com.auth0.jwt.exceptions.MissingClaimException;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
@@ -14,11 +10,11 @@ import java.io.IOException;
 
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws IOException {
         try {
             filterChain.doFilter(request, response);
         }
-        catch (TokenExpiredException | SignatureVerificationException | MissingClaimException e) {
+        catch (Exception e) {
             String message = switch (e.getClass().getSimpleName()) {
                 case "TokenExpiredException" -> "JWT is expired";
                 case "SignatureVerificationException" -> "JWT is not valid";
@@ -28,11 +24,6 @@ public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             response.getWriter().write(message);
-            response.getWriter().flush();
-        }
-        catch (RuntimeException e) {
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            response.getWriter().write("JWT is not valid");
             response.getWriter().flush();
         }
     }
