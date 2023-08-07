e2ddf0b7fd60f1c9370e330a8cb3769ac5abae3a
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2023-08-07 12:10:12 +0100

    Removing redundant code

--- src/main/java/gov/cabinetoffice/api/repositories/SubmissionRepository.java
+++ src/main/java/gov/cabinetoffice/api/repositories/SubmissionRepository.java
@@ -10,12 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface SubmissionRepository extends JpaRepository<Submission, UUID> {
-
-	List<Submission> findByApplicationGrantApplicationId(Integer applicationId);
-
 	List<Submission> findByStatusAndApplicationGrantApplicationId(SubmissionStatus status, Integer applicationId);
-
-	List<Submission> findBySchemeFunderIdAndSchemeGgisIdentifier(int funderId, String ggisIdentifier);
-
-	List<Submission> findBySchemeFunderId(int fundingOrgId);
 }
