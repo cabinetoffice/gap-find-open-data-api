a67d0881161e7e08327395f6a075dee0a04b5fbf
Antonio Lorusso <antonio.lorusso1@cabinetoffice.gov.uk>
2023-09-20 16:44:26 +0100

    fix userservice call to get email address

--- src/main/java/gov/cabinetoffice/api/dtos/user/UserDto.java
+++ src/main/java/gov/cabinetoffice/api/dtos/user/UserDto.java
@@ -1,5 +1,7 @@
 package gov.cabinetoffice.api.dtos.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,4 +10,10 @@ import lombok.Data;
 public class UserDto {
     private String gapUserId;
     private String emailAddress;
+
+    @JsonCreator
+    public UserDto(@JsonProperty("gapUserId") String gapUserId, @JsonProperty("emailAddress") String emailAddress) {
+        this.gapUserId = gapUserId;
+        this.emailAddress = emailAddress;
+    }
 }
