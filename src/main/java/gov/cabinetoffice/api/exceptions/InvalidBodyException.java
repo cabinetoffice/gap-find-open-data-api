c46bbf415df29ff2a267a3cb14fd393bed811d09
a-lor-cab <107372333+a-lor-cab@users.noreply.github.com>
2023-09-27 12:01:51 +0100

    wrap call to user service in a try catch. (#51)

---
+++ src/main/java/gov/cabinetoffice/api/exceptions/InvalidBodyException.java
@@ -0,0 +1,8 @@
+package gov.cabinetoffice.api.exceptions;
+
+public class InvalidBodyException extends RuntimeException {
+
+        public InvalidBodyException(String message) {
+            super(message);
+        }
+}
